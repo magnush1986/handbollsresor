a5ba769aeefb871fda838ff72aecf1e5bf33d449
magnush1986 <magnus.hemmen@gmail.com>
2025-05-27 23:53:23 +0200

    Update data.js

--- data.js
+++ data.js
@@ -454,7 +454,7 @@ function renderEventCard(e, target, isFirst = false) {
       <h3>Grundläggande info</h3>
       <div class="event-line"><span class="icon">🏷️</span><span class="label">Typ:</span> <span class="value">${e['Typ av händelse']}</span></div>
       <div class="event-line"><span class="icon">📍</span><span class="label">Plats:</span> <span class="value">${e['Plats']}</span></div>
-      <div class="event-line"><span class="icon">📅</span><span class="label">Period:</span> <span class="value">${e['Datum från']} – ${e['Datum till']}</span></div>
+      <div class="event-line"><span class="icon">🗓️</span><span class="label">Period:</span> <span class="value">${e['Datum från']} – ${e['Datum till']}</span></div>
       ${kostnadHtml}
       ${övrigInformationHtml}
     </div>
